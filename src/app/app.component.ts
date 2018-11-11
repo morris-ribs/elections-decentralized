c3dee1126bbb30e761855aeb3b66176eebe9c46b
morris-ribs <maumribeiro@gmail.com>
2018-11-11 17:57:07 +0100

    Adding radio buttons and button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'elections-decentralized';
+  title = 'Make your choice!';
+  favoriteSeason: string;
+  seasons: string[] = ['Winter', 'Spring', 'Summer', 'Autumn'];
 }
